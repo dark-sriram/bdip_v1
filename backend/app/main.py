90872b71d5ef83d3f42c7f2a51e5776ddf2276a6
dark-sriram <163014346+dark-sriram@users.noreply.github.com>
2026-05-26 14:48:47 +0530

    update the main and fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bdip.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
